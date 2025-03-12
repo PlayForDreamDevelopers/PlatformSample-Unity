a491d8b6cdead3f7c0d71422ae81ed657fb5a232
jiaoshiguan <jiaoshiguan@yvr.cn>
2025-03-12 11:48:21 +0800

    feat: update packages and UnityVersion
    [Module]: Version
    DetailInfo: 更新 com.yvr.platform 到 git 版本，更新 UnityEngine 版本到 2022.3.5f1

--- Assets/Common/DebugPanel/DebugManager.cs
+++ Assets/Common/DebugPanel/DebugManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using UnityEngine;
+using YVR.Utilities;
 
 public class DebugManager : MonoBehaviorSingleton<DebugManager>
 {
